d19f51c7d59969e5d46775434353fb9f7654eb15
Boimen <fgbaumann@icloud.com>
2022-05-08 00:09:03 -0300

    /

--- Rutas/RutaProductos.js
+++ Rutas/RutaProductos.js
@@ -1,13 +1,44 @@
 const express = require ('express')
 const Router = require ('express')
 const {serverpage,mostrarproductos,buscarprodNombre,borrarproducto,modificarproducto,guardarproducto} = require ('../Controllers/Productos')
+const {graphqlHTTP} = require ('express-graphql')
+const {buildSchema} = require ('graphql')
+const crypto = require ('crypto')
 
-
+const schema = buildSchema(`
+    type producto {
+        title: String,
+        price: Float
+    }
+    input productoinput {
+        id:ID!
+        title:String
+        price:Float
+        thumbnail:String
+        stock:Int
+    }
+    type Query {
+        mostrarproductos:producto
+        buscarprodNombre(title:String):[producto]
+    }
+    type Mutation{
+        guardarproducto(objeto:productoinput):[producto]
+    }
+    `
+);
 const RouterProductos = new Router ()
 
 
 RouterProductos.use(express.json());
 RouterProductos.use(express.urlencoded({ extended: true }));
+
+RouterProductos.use('/graphql' , graphqlHTTP({
+    schema:schema,
+    rootValue: {
+        mostrarproductos
+    },
+    graphiql:true,
+}))
 
 RouterProductos.get('/', serverpage)
 
